ca842cf9e56a1a10d553e7f732bdf5398c9b460d
Adarsh Jain <160226331+Adarshjain2001@users.noreply.github.com>
2025-03-07 19:35:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "4707e76408294ec7a52c329e45bf69f1";
+const API_KEY = process.env.API_KEY;
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("Latest"));
